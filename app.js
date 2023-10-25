7516a4fd8afe811f42c13b7fe48f7cdea0e26b59
adam <115082272+adama007@users.noreply.github.com>
2023-10-25 23:19:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Exemple de message "Hello" en JavaScript</title>
-</head>
-<body>
+
     <script>
-        // Utilisez la fonction 'document.write' pour afficher le message dans la page HTML
-        document.write("Hello");
+     console.log("Hello, Jenkins!");
     </script>
-</body>
-</html>
+
