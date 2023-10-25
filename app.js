13cc7f3950b90580ee2ecffe5c96334e8ce2df9d
adam <adam.chouk@polytechnicien.tn>
2023-10-25 22:12:30 +0100

    creation du fichier app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple de message "Hello" en JavaScript</title>
+</head>
+<body>
+    <script>
+        alert("Hello");
+    </script>
+</body>
+</html>
